1024f5b191a34dd56d0354223fd8a960762b8b7e
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-07-30 01:41:55 +0700

    Add function the_answer

--- deep/deep.py
+++ deep/deep.py
@@ -1,1 +1,13 @@
-#Placeholder
+def main():
+    the_answer(None)
+        
+def the_answer(everything):
+    answer = input('What is the answer to the Great Question of Life, the Universe and Everything? ')
+    everything = answer.strip().lower()
+    if everything == '42' or everything == 'forty-two' or everything == 'forty two':
+        print('Yes')
+    else:
+        print('No')
+    return
+
+main()
