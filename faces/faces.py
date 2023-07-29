2d184479918f719a4e1a39193c5d52d730c14e94
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-07-29 21:16:05 +0700

    Change convert function to use 2 replace()

---
+++ faces/faces.py
@@ -0,0 +1,10 @@
+def main():
+    convert('')
+    
+def convert(emoji):
+    emoji = input('')
+    return print(emoji.replace(':)', '🙂').replace(':(', '🙁'))
+
+main()
+            
+                
