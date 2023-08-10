f0b07cf6826c378187aeb6bb8744a2ae2917255f
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-11 01:43:02 +0700

    Add snake_case function to convert camel case to snake case

---
+++ camel/camel.py
@@ -0,0 +1,14 @@
+def main():
+    camel_case = input('camelCase: ')
+    snake_case(camel_case)
+    
+def snake_case(camel_case):
+    convert_case = ''
+    for i in camel_case:
+        if i.isupper():
+            convert_case +='_' + i.lower()
+        else:
+            convert_case += i
+    return convert_case
+
+main()
