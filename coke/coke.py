0307cf87caab2f7d7134553acac0f7ab6056e43f
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-11 02:02:35 +0700

    Add initial code to function. Need to revise.

---
+++ coke/coke.py
@@ -0,0 +1,10 @@
+def main():
+    insert_coin()
+
+def insert_coin(coin):
+    while  < 50:
+        amount_due = 50
+        print(amount_due)
+        coin = int(input('Insert Coin, only in 5, 10, or 25 cents : '))
+        
+        
