103bee81bd421bcd895933b0d6743c4c9ad42244
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-13 03:20:05 +0700

    Change print function to direct print after input in terminal without .py prompting user using sys lib after learning from cs50p

--- print_cs50.py
+++ print_cs50.py
@@ -1,14 +1,17 @@
+import sys
+
 def main():
-    print_cs50('Retry')
+    print_cs50()
     
-def print_cs50(problem):
-    problem = input('What is the name of the problem?\n')
+def print_cs50():
+    if len(sys.argv) < 2:
+        sys.exit('Too few arguments')
+    elif len(sys.argv) > 2:
+        sys.exit('Too many arguments')
     check50 = 'check50 cs50/problems/2022/python/'
     submit50 = 'submit50 cs50/problems/2022/python/'
-    print(check50 + problem.strip().lower())
-    print(submit50 + problem.strip().lower())
+    print(check50, sys.argv[1], sep='')
+    print(submit50, sys.argv[1], sep='')
     return
 
 main()
-    
-    
