815cd387957835c3c8aaf1126bf932113f0ecb7b
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-15 01:06:53 +0700

    Add working function to remove vowels to twttr.py

---
+++ twttr/twttr.py
@@ -0,0 +1,15 @@
+def main():
+    sentence = str(input('Input: '))
+    remove_vowels(sentence)
+    
+def remove_vowels(sentence):
+    no_vowels_sentence = ''
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for i in sentence:
+        if i.lower() in vowels:
+            del i
+        else:
+            no_vowels_sentence += i
+    return print('Output: ' + no_vowels_sentence)
+
+main()
