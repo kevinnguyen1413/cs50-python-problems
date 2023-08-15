eb2cf3a076b050fd6498ff2642838a7f633abfc8
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-16 03:02:39 +0700

    Create multiple functions for is_valid to check plates

--- plates/plates.py
+++ plates/plates.py
@@ -7,38 +7,36 @@ def main():
     else:
         print("Invalid")
 
-
-"""“All vanity plates must start with at least two letters.”
-“… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
-“Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. 
-The first number used cannot be a ‘0’.”
-“No periods, spaces, or punctuation marks are allowed.”"""
-
 def is_valid(s):
-    if s[0].isalpha() and s[1].isalpha():
+    if (start_alpha(s) and plate_length(s) and num_after_alpha(s) and first_non_zero(s) and no_punctuations(s)) == True:
         return True
+    return False
 
-    
+def start_alpha(s):
+    if s[0:2].isalpha():
+        return True
+    return False
+
+def plate_length(s):
     if len(s) in range(2, 7):
         return True
-    elif len(s) not in range(2,7):
-        return False
-    
+    return False
+
+def no_punctuations(s):
+    x = list(string.punctuation)
     for i in s:
-        if i == string.punctuation:
+        if i in x:
             return False
-        else:
-            return True
+    return True
+
+def first_non_zero(s):
     if s[2] != '0':
         return True
-    
-    if s[2:5].isnumeric() and s[6].isalpha():
-        return False
-    elif s[2:4].isnumeric() and s[5:6].isalpha():
-        return False
-    elif s[2:3].isnumeric() and s[4:6].isalpha():
-        return False
-    else:
-        return True
+    return False
 
+def num_after_alpha(s):
+    if any(s[i].isnumeric() for i in range(2, len(s))) and s[:-1].isalpha():
+        return False
+    return True
+    
 main()
