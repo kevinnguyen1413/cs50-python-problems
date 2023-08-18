80832e7e934d26f27a829d30726d119a767ef49e
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-19 01:23:39 +0700

    Change string Avocado California and remove break from else to prevent breaking after iteration

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,5 +1,5 @@
-fruits_cal = {'Apple': '130', 'Avocado California':'50', 'Banana':'110', 'Cantaloupe':'50', 'Grapefruit':'60', 'Grapes':'90', 'Honeydew Melon':'50', 'Kiwifruit':'90',\
-    'Lemon': '15', 'Lime': '20', 'Orange': '80', 'Nectarine': '60', 'Peach': '60', 'Pear': '100', 'Pineapple': '50', 'Plums': '70',\
+fruits_cal = {'Apple': '130', 'Avocado':'50', 'Banana':'110', 'Cantaloupe':'50', 'Grapefruit':'60', 'Grapes':'90', 'Honeydew Melon':'50', 'Kiwifruit':'90',
+    'Lemon': '15', 'Lime': '20', 'Orange': '80', 'Nectarine': '60', 'Peach': '60', 'Pear': '100', 'Pineapple': '50', 'Plums': '70',
     'Strawberries': '50', 'Sweet Cherries': '100', 'Tangerine': '50', 'Watermelon': '80'}
 
 def main():
@@ -11,7 +11,6 @@ def check_calories(s):
         if s == k.lower():
             print('Calories:', v)
         else:
-            print('Please enter a fruit in FDA raw fruits list')
-            break
+            None
 
 main()
