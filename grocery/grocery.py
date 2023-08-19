8bcd49a94be0b3cff7e0aa90f3c36cfa18c97911
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-20 02:27:58 +0700

    Create grocery_list function to return count and items need to buy

---
+++ grocery/grocery.py
@@ -0,0 +1,19 @@
+def main():
+    grocery_list()
+
+def grocery_list():
+    to_buy_dict = {}
+    while True:
+        try:
+            item = input('').lower()
+            if item in to_buy_dict:
+                to_buy_dict[item] += 1
+            else:
+                to_buy_dict[item] = 1
+        except EOFError:
+            for item in sorted(to_buy_dict):
+                print(to_buy_dict[item], item.upper())
+            break
+
+main()
+                
