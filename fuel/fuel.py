c0feeea965da9291f442b665cb6a343e51caf0fa
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-20 00:53:45 +0700

    Create fraction_convert function with exception handling

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,26 @@
 def main():
-    num = int(input('Fraction: '))
-    fraction_convert(num)
+    fraction_convert()
     
-def fraction_convert(fraction):
-    fraction = 
-        
+def fraction_convert():
+    while True:
+        try:
+            frac = input('Fraction: ').split('/')
+            num = int(frac[0])
+            denom = int(frac[1])
+            percentage = round(num/denom*100)
+        except ValueError:
+            continue
+        except ZeroDivisionError:
+            continue
+        if num > denom:
+            continue
+        else:
+            if percentage <= 1:
+                print('E')
+            elif percentage >= 99:
+                print('F')
+            else:
+                print(f'{percentage}%')
+        return
+                
+main()
