576d77cdc70f8f9d77b6e2814e739ca42afccf59
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-21 02:56:29 +0700

    Create conversion from mm-dd-yyyy to yyyy-mm-dd

---
+++ outdated/outdated.py
@@ -0,0 +1,49 @@
+def main():
+    int_date_conversion()
+    
+months_in_year = {
+    "January": '01',
+    "February": '02',
+    "March": '03',
+    "April": '04',
+    "May": '05',
+    "June": '06',
+    "July": '07',
+    "August": '08',
+    "September": '09',
+    "October": '10',
+    "November": '11',
+    "December": '12'
+}
+
+def int_date_conversion():
+    while True:
+        mdy = input('Date: ')
+        if mdy.split()[0] in months_in_year and ',' in mdy.split()[1]:
+            mdy = mdy.split()
+            for word in mdy:
+                if word in months_in_year:
+                    mdy[0] = months_in_year[word]
+                    mdy[1] = mdy[1].replace(',', '')
+        elif '/' in mdy:
+                mdy = mdy.split('/')
+        else:
+            continue
+        
+        try:
+            year = int(mdy[2])
+            month = int(mdy[0])
+            date = int(mdy[1])
+            if month not in range(1, 13) or date not in range(1, 32):
+                continue
+        except IndexError:
+            continue
+        except ValueError:
+            continue
+        else:
+            break
+
+    conversion = f'{year:04}-{month:02}-{date:02}'
+    print(conversion)
+
+main()
