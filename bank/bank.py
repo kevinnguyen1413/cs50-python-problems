fad00cb382da62f8fca145f47af891f7036b7e6b
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-08-22 20:20:50 +0700

    Clean up code

--- bank/bank.py
+++ bank/bank.py
@@ -7,8 +7,6 @@ def greet(sentence):
         return print('$0')
     elif list(sentence)[0] == 'h':
         return print('$20')
-    else:
-        return print('$100')
-    return
+    return print('$100')
 
 main()
