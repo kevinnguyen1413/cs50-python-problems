7a3eb7fc75afeb90d20b6dda672033083679f0e8
Kevin Nguyen <kevinnguyen1413@gmail.com>
2023-09-01 01:08:01 +0700

    Create error handling for float conversion and improper input

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,12 +1,22 @@
 import requests
 import sys
 
+if len (sys.argv) != 2:
+    print('Missing command-line argument')
+    sys.exit()
+try:
+    btc_amount = float(sys.argv[1])
+except ValueError:
+    print('Command-line argument is not a number')
+
 
 def main():
     bitcoin_price()
-    
+
 def bitcoin_price():
     try:
-        
+        print('Hello')
     except requests.RequestException:
+        print('hello')
         
+main()
